a41d5538e799bd0e541002792dc63aab73759df5
Ece Bayraktar <ecebyrktr1@gmail.com>
2024-11-28 15:32:37 +0300

    IDEFIX
    Web teknolojileri projesi

---
+++ pages/CarouselPage.vue
@@ -0,0 +1,41 @@
+
+<template>
+  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+<ol class="carousel-indicators">
+  <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+  <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+  <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+</ol>
+<div class="carousel-inner">
+  <div class="carousel-item active">
+    <img class="d-block w-100" src="https://images.idefix.com/mpstatics/banner/test-67239a5a91787.webp" alt="First slide">
+  </div>
+  <div class="carousel-item">
+    <img class="d-block w-100" src="https://images.idefix.com/mpstatics/banner/test-6724b46b9490e.webp" alt="Second slide">
+  </div>
+  <div class="carousel-item">
+    <img class="d-block w-100" src="https://images.idefix.com/mpstatics/banner/test-6723981463da3.webp" alt="Third slide">
+  </div>
+</div>
+<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+  <span class="sr-only">Previous</span>
+</a>
+<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+  <span class="sr-only">Next</span>
+</a>
+</div>
+   
+</template>
+
+<script>
+export default {
+  name: 'CarouselPage'
+}
+definePageMeta({
+layout: 'default',
+});
+</script>
+
+
